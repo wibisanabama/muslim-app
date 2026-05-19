7fd254f8159f84152a1300d6e9c798ffd73a3245
wibisanabama <wibisanabama@gmail.com>
2026-05-19 19:56:29 +0700

    feat: initialize application with MultiProvider and ShalatPage navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'repository/shalat_repository.dart';
+import 'viewmodel/shalat_view_model.dart';
+import 'view/shalat_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        Provider<ShalatRepository>(
+          create: (_) => ShalatRepository(),
         ),
+        ChangeNotifierProvider<ShalatViewModel>(
+          create: (context) => ShalatViewModel(
+            context.read<ShalatRepository>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Jadwal Shalat MVVM',
+        theme: ThemeData(useMaterial3: true),
+
+        // Langsung ke ShalatPage
+        home: const ShalatPage(),
       ),
     );
   }
