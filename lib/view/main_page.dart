911536460cc23f9d1ce0ef7229c52b79b7f0141c
wibisanabama <wibisanabama@gmail.com>
2026-05-25 18:16:41 +0700

    feat: globalize drawer management to cover navigation bar

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -4,6 +4,8 @@ import 'shalat_page.dart';
 import 'quran_page.dart';
 import 'doa_page.dart';
 import 'about_page.dart';
+import 'muslim_drawer.dart';
+import 'auth_dialogs.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -13,23 +15,29 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   int _currentIndex = 0;
 
-  final List<Widget> _pages = [
-    const HomePage(),
+  late final List<Widget> _pages;
 
-    const ShalatPage(),
-
-    const QuranPage(),
-
-    const DoaPage(),
-
-    const AboutPage(),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      HomePage(scaffoldKey: _scaffoldKey),
+      ShalatPage(scaffoldKey: _scaffoldKey),
+      QuranPage(scaffoldKey: _scaffoldKey),
+      DoaPage(scaffoldKey: _scaffoldKey),
+      AboutPage(scaffoldKey: _scaffoldKey),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: const MuslimDrawer(),
+      endDrawer: const ProfileDrawer(),
       body: _pages[_currentIndex],
       bottomNavigationBar: NavigationBar(
         selectedIndex: _currentIndex,
