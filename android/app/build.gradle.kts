8a5c19b597bb1711991d318ceb3a6d891c9430ce
wibisanabama <wibisanabama@gmail.com>
2026-05-27 17:03:27 +0700

    build: upgrade Android compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "id.muslimapp.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -35,7 +35,7 @@ android {
     defaultConfig {
         applicationId = "id.muslimapp.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
